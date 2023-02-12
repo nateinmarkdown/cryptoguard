ef50a60ca6fcb2b9bea9bbe44a864c7356db31d1
Nate <121343177+nateinmarkdown@users.noreply.github.com>
2023-02-12 10:29:50 +0100

    Create cryptoguard.py

---
+++ cryptoguard.py
@@ -0,0 +1,81 @@
+import os
+import sys
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, hmac
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers import (
+    Cipher, algorithms, modes
+)
+
+backend = default_backend()
+
+def encrypt_file(key, filename):
+    # Check that key is the correct length for AES
+    if len(key) != 32:
+        raise ValueError("Key must be 32 bytes long")
+
+    # Generate a random initializing vector (IV)
+    iv = os.urandom(16)
+
+    encryptor = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend).encryptor()
+
+    # Read the contents of the file to be encrypted
+    with open(filename, "rb") as f:
+        plaintext = f.read()
+
+    # Pad the plaintext to a multiple of 16 bytes
+    padding_length = 16 - (len(plaintext) % 16)
+    plaintext += bytes([padding_length]) * padding_length
+
+    # Encrypt the plaintext
+    ciphertext = encryptor.update(plaintext) + encryptor.finalize()
+
+    # Write the encrypted data to a file
+    with open(filename + ".encrypted", "wb") as f:
+        f.write(iv + ciphertext)
+
+def decrypt_file(key, filename):
+    # Check that key is the correct length for AES
+    if len(key) != 32:
+        raise ValueError("Key must be 32 bytes long")
+
+    # Read the IV and ciphertext from the encrypted file
+    with open(filename, "rb") as f:
+        iv = f.read(16)
+        ciphertext = f.read()
+
+    decryptor = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend).decryptor()
+
+    # Decrypt the ciphertext
+    plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+
+    # Remove padding from the plaintext
+    padding_length = plaintext[-1]
+    plaintext = plaintext[:-padding_length]
+
+    # Write the decrypted data to a file
+    with open(filename[:-10], "wb") as f:
+        f.write(plaintext)
+
+def main():
+    # Check that the correct number of arguments have been provided
+    if len(sys.argv) != 4:
+        raise ValueError("Usage: python3 CryptoGuard.py [encrypt/decrypt] [key] [file]")
+
+    # Parse the command line arguments
+    command = sys.argv[1]
+    key = sys.argv[2].encode("utf-8")
+    filename = sys.argv[3]
+
+    if command == "encrypt":
+        encrypt_file(key, filename)
+    elif command == "decrypt":
+        decrypt_file(key, filename)
+    else:
+        raise ValueError("Usage: python3 CryptoGuard.py [encrypt/decrypt] [key] [file]")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except ValueError as e:
+        print("Error:", e)
